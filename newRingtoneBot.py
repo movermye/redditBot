442cbe7ff21ea19da215469c3134692fbdaecfc1
mark <movermy@gmail.com>
2016-08-13 22:48:12 -0400

    basic success commenting with newRingtone!

--- newRingtoneBot.py
+++ newRingtoneBot.py
@@ -14,17 +14,17 @@ def bot_action(c, verbose=True, respond=False):
     print "attempting to comment"
     c.reply("It worked!")
 
-if __name__ is '__main__':
-    import praw
-    import OAuth2Util
-    r = praw.Reddit('basic call response bot example by SkylineBilly')
-    o = OAuth2Util.OAuth2Util(r, print_log=True)
-    o.refresh(force=True)
 
-    targetSubreddit = 'billysTestReddit'
+import praw
+import OAuth2Util
+r = praw.Reddit('basic call response bot example by SkylineBilly')
+o = OAuth2Util.OAuth2Util(r, print_log=True)
+o.refresh(force=True)
 
-    for c in praw.helpers.comment_stream(r, targetSubreddit):
-        if check_condition(c):
-            # set 'respond=True' to activate bot responses. Must be logged in.
-            print "deploying bot action"
-            bot_action(c, respond=True)
+targetSubreddit = 'billysTestReddit'
+
+for c in praw.helpers.comment_stream(r, targetSubreddit):
+    if check_condition(c):
+        # set 'respond=True' to activate bot responses. Must be logged in.
+        print "deploying bot action"
+        bot_action(c, respond=True)
