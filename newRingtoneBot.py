5127384990d7e3aade044fdc1c3480b997580a52
mark <movermy@gmail.com>
2016-08-13 22:45:51 -0400

    commiting before radical changes to newRingtoneBot

--- newRingtoneBot.py
+++ newRingtoneBot.py
@@ -1,51 +1,30 @@
 
-#import praw
- 
-
-
-#r = praw.Reddit(user_agent="USER AGENT")
-#r.login('SkylineBilly', 'roooo00oo')
-
-targetSubreddit = 'billysTestReddit'
-
-#import re
-
 def check_condition(c):
+    print "inside test condition"
+    print "printing text:"
     text = c.body
+    print text
+    print "printing tokens:"
     tokens = text.split()
-    if "1)" in tokens and "2)" in tokens:
-        return True
+    print tokens
+    return True
 
 def bot_action(c, verbose=True, respond=False):
-    fixed = re.sub(r'(\n?)([0-9]+)(\))', r'\n\n\2.', c.body)
-
-    if verbose:
-        print c.body.encode("UTF-8")
-        print "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
-        print fixed.encode("UTF-8")
-
-    if respond:
-        head = "Hi! Let me try to beautify the list in  your comment:\n\n"
-        tail = "\n\nI am a bot. You can provide feedback in my subreddit: /r/ListFormatFixer"
-        c.reply(head + fixed + tail)
+    print "inside bot_action"
+    print "attempting to comment"
+    c.reply("It worked!")
 
 if __name__ is '__main__':
     import praw
-    r = praw.Reddit(UA)
+    import OAuth2Util
+    r = praw.Reddit('basic call response bot example by SkylineBilly')
+    o = OAuth2Util.OAuth2Util(r, print_log=True)
+    o.refresh(force=True)
 
-    # Provide a descriptive user-agent string. Explain what your bot does, reference
-    # yourself as the author, and offer some preferred contact method. A reddit
-    # username is sufficient, but nothing wrong with adding an email in here.
-    UA = 'ListFormatFixer praw demo, Created by /u/shaggorama'
-
-    # If you want the bot to be able to respond to people, you will need to login.
-    # It is strongly recommended you login with oAuth
-    # http://praw.readthedocs.org/en/stable/pages/oauth.html
-
-    # NB: This login method is being deprecated soon
-    r.login()
+    targetSubreddit = 'billysTestReddit'
 
     for c in praw.helpers.comment_stream(r, targetSubreddit):
         if check_condition(c):
             # set 'respond=True' to activate bot responses. Must be logged in.
-            bot_action(c, respond=False)
+            print "deploying bot action"
+            bot_action(c, respond=True)
